0e7b7fe90607ad856479a1cf450c4b2799a0be32
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-01-05 17:20:10 +0100

    Fix weapon rendering

--- src/main/java/dev/padjokej/gamblersfallacy/utils/ModModelPredicates.java
+++ src/main/java/dev/padjokej/gamblersfallacy/utils/ModModelPredicates.java
@@ -5,23 +5,22 @@
 package dev.padjokej.gamblersfallacy.utils;
 
 import dev.padjokej.gamblersfallacy.GamblersFallacy;
+import dev.padjokej.gamblersfallacy.component.ModDataComponentTypes;
 import dev.padjokej.gamblersfallacy.items.GamblingWeapon;
 import dev.padjokej.gamblersfallacy.items.ModWeapons;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
 
 public class ModModelPredicates {
     public static void registerModelPredicateProviders() {
         ModelPredicateProviderRegistry.register(ModWeapons.GAMBLING_WEAPON, Identifier.of(GamblersFallacy.MOD_ID, "state"),
                 (stack, world, entity, seed) -> {
-
-            if (stack.getItem() instanceof GamblingWeapon gamblingWeapon) {
-                return gamblingWeapon.getState();
+            if (stack.get(ModDataComponentTypes.WEAPON_STATE) != null)
+                return stack.get(ModDataComponentTypes.WEAPON_STATE).get(Random.create());
+            else
+                return 0f;
             }
-            if (entity == null) {
-                return 0;
-            }
-            return 0;
-        });
+        );
     }
 }
